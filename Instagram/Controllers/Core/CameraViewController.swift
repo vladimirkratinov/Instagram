fc7fcd6a6fda54d21d3762f35d5729130b763730
Vladimir Kratinov <vladimirkratinov@gmail.com>
2023-03-01 21:34:54 -0800

    Add photo capture to Camera Module

--- Instagram/Controllers/Core/CameraViewController.swift
+++ Instagram/Controllers/Core/CameraViewController.swift
@@ -10,16 +10,33 @@ import AVFoundation
 
 class CameraViewController: UIViewController {
     
-    private var output = AVCapturePhotoOutput()
+    // Capture session:
     private var captureSession: AVCaptureSession?
+    // Photo Output:
+    private var output = AVCapturePhotoOutput()
+    // Video preview:
     private let previewLayer = AVCaptureVideoPreviewLayer()
+    // Shutter button:
+    private let shutterButton: UIButton = {
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        button.layer.cornerRadius = 50
+        button.layer.borderWidth = 10
+        button.layer.borderColor = UIColor.white.cgColor
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .black
         title = "Take Photo"
+        
+        view.layer.addSublayer(previewLayer)
+        view.addSubview(shutterButton)
+        
         setUpNavBar()
         checkCameraPermission()
+        
+        shutterButton.addTarget(self, action: #selector(didTapTakePhoto), for: .touchUpInside)
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -33,12 +50,16 @@ class CameraViewController: UIViewController {
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        previewLayer.frame = CGRect(
-            x: 0,
-            y: view.safeAreaInsets.top,
-            width: view.width,
-            height: view.width
-        )
+//        previewLayer.frame = CGRect(
+//            x: 0,
+//            y: view.safeAreaInsets.top,
+//            width: view.width,
+//            height: view.width
+//        )
+        
+        previewLayer.frame = view.bounds
+        shutterButton.center = CGPoint(x: view.frame.size.width/2,
+                                       y: view.frame.size.height - 100)
     }
     
     override func viewDidDisappear(_ animated: Bool) {
@@ -46,20 +67,27 @@ class CameraViewController: UIViewController {
         captureSession?.stopRunning()
     }
     
-    @objc func didTapClose() {
+    @objc private func didTapTakePhoto() {
+        output.capturePhoto(with: AVCapturePhotoSettings(),
+                            delegate: self)
+    }
+    
+    @objc private func didTapClose() {
         tabBarController?.selectedIndex = 0
         tabBarController?.tabBar.isHidden = false
     }
     
+    // Check camera permission:
     private func checkCameraPermission() {
         switch AVCaptureDevice.authorizationStatus(for: AVMediaType.video) {
             
         case .notDetermined:
-            // request
+            // Request a permission:
             AVCaptureDevice.requestAccess(for: .video) { [weak self] granted in
                 guard granted else {
                     return
                 }
+                // Call Set Up Camera (UI) in the Main Thread:
                 DispatchQueue.main.async {
                     self?.setUpCamera()
                 }
@@ -80,6 +108,7 @@ class CameraViewController: UIViewController {
         // Add Device:
         if let device = AVCaptureDevice.default(for: .video) {
             do {
+                // Create input from the device:
                 let input = try AVCaptureDeviceInput(device: device)
                 if captureSession.canAddInput(input) {
                     // Add Input:
@@ -90,6 +119,7 @@ class CameraViewController: UIViewController {
                 print(error)
             }
             
+            // Add output:
             if captureSession.canAddOutput(output) {
                 captureSession.addOutput(output)
             }
@@ -97,11 +127,11 @@ class CameraViewController: UIViewController {
             // Layer
             previewLayer.session = captureSession
             previewLayer.videoGravity = .resizeAspectFill
-            view.layer.addSublayer(previewLayer)
             
             // Background Thread UI Issue:
             DispatchQueue.global().async {
                 captureSession.startRunning()
+                self.captureSession = captureSession
             }
         }
     }
@@ -115,5 +145,20 @@ class CameraViewController: UIViewController {
         navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
         navigationController?.navigationBar.shadowImage = UIImage()
         navigationController?.navigationBar.backgroundColor = .clear
+    }
+}
+
+extension CameraViewController: AVCapturePhotoCaptureDelegate {
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        // Convert photo to data:
+        guard let data = photo.fileDataRepresentation() else { return }
+        let image = UIImage(data: data)
+        
+        captureSession?.stopRunning()
+        
+        let imageView = UIImageView(image: image)
+        imageView.contentMode = .scaleToFill
+        imageView.frame = view.bounds
+        view.addSubview(imageView)
     }
 }
